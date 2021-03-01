907236799ec86a95d0983dc377511efadbd19438
arturo101199 <al375559@uji.es>
2021-03-01 22:11:17 +0100

    Position and rotation of characters on main menu

---
+++ Assets/Scripts/MainMenu/DivideCircle.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class DivideCircle : MonoBehaviour
+{
+    [SerializeField] Transform[] elements;
+    [SerializeField] float radius = 5;
+
+    void Start()
+    {
+        float angle = 360 / elements.Length * Mathf.Deg2Rad;
+        for (int i = 0; i < elements.Length; i++)
+        {
+            CalculatePositionAndRotation(angle, i);
+        }
+    }
+
+    void CalculatePositionAndRotation(float angle, int index)
+    {
+        float t = angle * index;
+        float x = radius * Mathf.Cos(t);
+        float z = radius * Mathf.Sin(t);
+        Vector3 position = new Vector3(x, 0, z);
+        Quaternion lookAt = Quaternion.LookRotation(position);
+        elements[index].SetPositionAndRotation(position, lookAt);
+    }
+
+}
