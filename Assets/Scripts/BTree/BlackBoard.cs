d7bbb1ebee831ab4abb7c17bac1e774fa1cbae82
arturo101199 <al375559@uji.es>
2021-03-02 20:11:08 +0100

    Basic BlackBoard Setup

---
+++ Assets/Scripts/BTree/BlackBoard.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class BlackBoard : MonoBehaviour
+{
+    Dictionary<string, object> blackBoard = new Dictionary<string, object>();
+
+    public void AddKeyValue(string key, object value)
+    {
+        blackBoard.Add(key, value);
+    }
+
+    public void UpdateValue(string key, object value)
+    {
+        blackBoard[key] = value;
+    }
+
+    public object GetValue(string key)
+    {
+        object value;
+        bool found = blackBoard.TryGetValue(key, out value);
+        if (!found)
+        {
+            Debug.LogError("Not found " + key);
+        }
+        return value;
+    }
+}
