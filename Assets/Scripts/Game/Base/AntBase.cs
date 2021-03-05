25486d31f1cb7a1ac2fc7a93a806211314c98747
arturo101199 <al375559@uji.es>
2021-03-05 14:15:48 +0100

    Global Blackboard, bases and some other minor stuff

---
+++ Assets/Scripts/Game/Base/AntBase.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class AntBase : MonoBehaviour, IDamageable
+{
+    int totalNumberOfAnts;
+    AntGlobalBlackboard antGlobalBlackboard;
+    [SerializeField] float health;
+
+    void Start()
+    {
+        antGlobalBlackboard = AntGlobalBlackboard.Instance;
+        totalNumberOfAnts = (int)antGlobalBlackboard.GetValue("totalNumberOfAnts");
+    }
+
+    public void GetDamage(float damageValue)
+    {
+        health -= damageValue;
+    }
+
+}
---
+++ Assets/Scripts/Game/Base/AntGlobalBlackboard.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using UnityEngine;
+
+public class AntGlobalBlackboard : Blackboard
+{
+    private static AntGlobalBlackboard instance;
+
+    public static AntGlobalBlackboard Instance { get => instance; }
+
+    private void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            instance = this;
+        }
+        DontDestroyOnLoad(this);
+    }
+
+    void Start()
+    {
+        initializeBlackboard();
+    }
+
+    void initializeBlackboard()
+    {
+        AddKeyValue("resourcesEaten", 0);
+        AddKeyValue("antsPatrolling", 0);
+        AddKeyValue("totalNumberOfAnts", 0);
+        AddKeyValue("baseIsInDanger", false);
+        AddKeyValue("homeLocation", Vector3.zero);
+    }
+}
