2b1b1e59c9a983ce37c96fc6bb89fa047731cb76
arturo101199 <al375559@uji.es>
2021-03-20 20:23:57 +0100

    Healable

---
+++ Assets/Scripts/Healable.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Healable : MonoBehaviour, IHealable
+{
+    Blackboard blackboard;
+    [SerializeField] float maxHealth;
+
+    private void Awake()
+    {
+        blackboard = GetComponent<Blackboard>();
+    }
+
+    public void Heal(float value)
+    {
+       blackboard.UpdateValue("health", (float)blackboard.GetValue("health") + value);
+    }
+}
