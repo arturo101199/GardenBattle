63e5c61901fc21896e19bdb153a0ebc3b0657c59
arturo101199 <al375559@uji.es>
2021-03-28 19:52:47 +0200

    Some improvements on movement

--- Assets/Scripts/BTree/Actions/Ant/Patrolling/SearchBase/CheckDistanceToEnemyBase.cs
+++ Assets/Scripts/BTree/Actions/Ant/Patrolling/SearchBase/CheckDistanceToEnemyBase.cs
@@ -2,13 +2,15 @@
 
 public class CheckDistanceToEnemyBase : BNode
 {
-    float distanceToBase = 4f;
+    float distanceToBase = 5f;
 
     public override NodeState Evaluate()
     {
         Vector3 closerBase = (Vector3)blackboard.GetValue("closerEnemyBase");
         if (Vector3.Distance(transform.position, closerBase) < distanceToBase)
+        {
             return NodeState.SUCCESS;
+        }
         else
             return NodeState.FAIL;
     }
--- Assets/Scripts/BTree/Actions/Ant/Patrolling/SearchBase/CheckDistanceToNextPoint.cs
+++ Assets/Scripts/BTree/Actions/Ant/Patrolling/SearchBase/CheckDistanceToNextPoint.cs
@@ -4,18 +4,43 @@ using UnityEngine.AI;
 public class CheckDistanceToNextPoint : BNode
 {
     NavMeshAgent agent;
+    Transform rayOrigin;
+    LayerMask myLayer;
+    float timer = 0f;
+    bool colisioned;
 
     private void Start()
     {
         agent = (NavMeshAgent)blackboard.GetValue("navMeshAgent");
+        rayOrigin = GetComponentInChildren<Transform>();
+        myLayer = 1 << gameObject.layer;
     }
 
     public override NodeState Evaluate()
     {
         if (agent.remainingDistance < 1f || agent.remainingDistance == Mathf.Infinity)
+        {
             return NodeState.SUCCESS;
-        else
-            return NodeState.FAIL;
+        }
+        else if (Physics.Raycast(rayOrigin.position, transform.forward, 1.5f, myLayer))
+        {
+            if(!colisioned)
+            {
+                colisioned = true;
+                timer = 0f;
+                return NodeState.SUCCESS;
+            }
+        }
+        if (colisioned)
+        {
+            timer += Time.deltaTime;
+            if (timer >= 2)
+            {
+                colisioned = false;
+                timer = 0f;
+            }
+        }
+        return NodeState.FAIL;
     }
 
     public override void OnTreeEnded()
