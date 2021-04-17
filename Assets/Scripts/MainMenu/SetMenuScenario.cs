f6f0223a7c8d2d3466fc53aecc9d1a85815ac270
arturo101199 <al375559@uji.es>
2021-04-17 18:12:23 +0200

    Main menu in progress

--- Assets/Scripts/MainMenu/SetMenuScenario.cs
+++ Assets/Scripts/MainMenu/SetMenuScenario.cs
@@ -1,14 +1,16 @@
 ﻿using UnityEngine;
 
-public class PlaceMenuCharacters : MonoBehaviour
+public class SetMenuScenario : MonoBehaviour
 {
     [SerializeField] Transform[] elements = null;
     [SerializeField] float radius = 5;
     [SerializeField] Transform center = null;
+    [SerializeField] Transform camera = null;
 
     void Start()
     {
         placeMenuCharacters();
+        placeCloseCamera();
     }
 
     void placeMenuCharacters()
@@ -21,4 +23,11 @@ public class PlaceMenuCharacters : MonoBehaviour
         }
     }
 
+    void placeCloseCamera()
+    {
+        camera.position = elements[0].transform.position + (elements[0].transform.position - center.position).normalized * 2.2f;
+        camera.position = new Vector3(camera.position.x, 1.3f, camera.position.z);
+        Quaternion lookAt = Quaternion.LookRotation(center.position - camera.position);
+        camera.rotation = lookAt;
+    }
 }
