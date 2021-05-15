9641d8263ed26f99105768b0b1092284563e2603
arturo101199 <al375559@uji.es>
2021-05-15 20:30:35 +0200

    Added Settings and Music

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    AudioSource audio;
+    float maxVolume = 0f;
+
+    private void Awake()
+    {
+        audio = GetComponent<AudioSource>();
+    }
+
+    void Start()
+    {
+        maxVolume = audio.volume;
+        audio.volume = PlayerPrefs.GetFloat("Volume") * maxVolume;
+    }
+
+    public void SetVolume(float volume)
+    {
+        audio.volume = maxVolume * volume;
+    }
+}
---
+++ Assets/Scripts/AudioSlider.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class AudioSlider : MonoBehaviour
+{
+    [SerializeField] AudioManager audioManager;
+    Slider slider;
+
+    private void Awake()
+    {
+        slider = GetComponent<Slider>();
+    }
+
+    private void Start()
+    {
+        slider.value = PlayerPrefs.GetFloat("Volume");
+    }
+
+    public void OnValueChange(float value)
+    {
+        print(value);
+        PlayerPrefs.SetFloat("Volume", value);
+        audioManager.SetVolume(value);
+    }
+}
